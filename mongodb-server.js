2533983fc7dbcb622c1ffa7fec3e35c93b22f4af
sharozekkhan <sharozekkhan@gmail.com>
2023-05-04 09:59:00 +0500

    Implemented mongodb server

---
+++ mongodb-server.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const app = express();
+// const http = require("http");
+// const mysql = require('mysql');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+// const tm = require( 'text-miner');
+// const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
+const { connect } = require('http2');
+
+const port = process.env.PORT || 4128;
+
+// const server = http.createServer(app);
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// mongoose.set('strictQuery', true);
+// mongoose.set('strictQuery', true);
+// ip: localhost | 172.104.174.187
+// port: 27017
+// database: FirstDemo
+// mongoose.connect(`mongodb://localhost:27017/FirstDemo`);
+//Mongologs
+
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//   console.log('MongoDB database connection established successfully');
+//   isConnected = true;
+// });
+// const Schema = mongoose.Schema;
+
+// const studentSchema = new Schema({
+//   name: { type: String },
+//   age: { type: Number }
+// });
+// const Student = mongoose.model('students', studentSchema);
+
+app.post('/api/signup', async function (req, res) {
+  const { ipAdd, portNum, dbName } = req.body;
+  const uri = `mongodb://${ipAdd}:${portNum}/${dbName}`;
+
+  MongoClient.connect(uri, {useNewUrlParser: true}, function(err, client) {
+    if (err) {
+      return console.error(err);
+    }
+    const db = client.db(`${dbName}`);
+    const logs = db.collection('startup_log');
+    
+    logs.find().toArray(function(err, result) {
+      if (err) {
+        return console.error(err);
+      }
+      console.log(result);
+      // console.log("result");
+      client.close();
+      return res.send(result);
+    })
+  })
+  // mongoose.connect(`mongodb://${ipAdd}:${portNum}/${dbName}`);
+  // let isConnected = false;
+  // const fluffy = new Student({ name: 'sameer' });
+  // await fluffy.save();
+  
+  // const result
+  // mongoose.connect(`mongodb://localhost:27017/FirstDemo`);
+  // const result = await Student.find()
+  // res.send(result);
+});
+
+// app.get('/api/students', async function (req,res) {
+//   mongoose.connect(`mongodb://localhost:27017/FirstDemo`);
+//   const result = await Student.find({name:'Zain Raza'})
+//   res.send(result);
+// })
+
+app.listen(port, () => {
+  console.log(`App started on port: ${port}`);
+  // MongoClient.connect((`mongodb://localhost:27017`,
+  // {useNewUrlParser:true , useUnifiedTopology:true}))
+})
+
+
+// server.listen(3054, () => {
+//   console.log(`Socket port: ${3054}`)
+// })
+
