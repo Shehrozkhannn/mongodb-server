66f226b5a0299218cb580b986cb05e28b7d62f68
Muhammad Nasir <n_mahmood999@yahoo.com>
2023-05-07 20:27:42 +0500

    added body parsers for error req.body undefined

--- mongodb-server.js
+++ mongodb-server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const mysql = require('mysql');
 const mongoose = require('mongoose');
 const startups = require('./db.js');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const pool = mysql.createPool({
           connectionLimit: 10,
