79bc345900cbd931d695eb8d54da8418c7180f4b
Muhammad Nasir <n_mahmood999@yahoo.com>
2023-07-30 23:27:00 +0500

    Update mongodb-server.js
    (4)

--- mongodb-server.js
+++ mongodb-server.js
@@ -34,7 +34,7 @@ app.use(bodyParser.json());
 //                     });
 // });
 
-const mongoUrl = 'mongodb://mongouser:password@172.104.174.187:27017/local';
+const mongoUrl = 'mongodb://172.104.174.187:27017/local';
 
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
